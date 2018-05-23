63a4aea3b3129dde5d870552c4d52e65c7237553
Alvie Rahman <alvierahman90@gmail.com>
2018-05-23 17:49:55 +0100

    Formatting

--- .config/polybar/quotesbar.py
+++ .config/polybar/quotesbar.py
@@ -4,9 +4,10 @@ import json
 import sys
 import time
 
-SPEED=5
-TWEETERS=['billwurtz','deiuge']
-MAX_LENGTH=100
+SPEED = 5
+TWEETERS = ['billwurtz', 'deiuge']
+MAX_LENGTH = 100
+
 
 def auth_twitter():
     import twitter
@@ -21,19 +22,21 @@ def auth_twitter():
         ))
     return t
 
-def add_quotes_from_twitter(t,user):
-    tweets = t.statuses.user_timeline(screen_name=user,include_rts=False)
-    quotes = get_quotes()
-    
+
+def add_quotes_from_twitter(t, user):
+    tweets = t.statuses.user_timeline(screen_name=user, include_rts=False)
+
     for i in tweets:
-        i['text'] = i['text'].replace('\n','    ')
+        i['text'] = i['text'].replace('\n', '    ')
         write_quote(i)
+
 
 def get_quotes():
     output = []
     with open('/home/alvie/.config/polybar/quotes') as f:
         output = json.loads(f.read())
     return output
+
 
 # overwrite the whole thing as opposed to adding one
 def write_quotes(quotes):
@@ -53,6 +56,7 @@ def write_quote(quote):
     quotes[quote['id']] = quote
     write_quotes(quotes)
 
+
 # returns deleted message on success, return None if quote isn't found
 def remove_quote(quote_id):
     quotes = get_quotes()
@@ -60,34 +64,39 @@ def remove_quote(quote_id):
     write_quotes(quotes)
     return deleted
 
+
 # this could probably be implemented by making a list from get_quotes then
 # shuffling that in the main() function in the one place it's needed
 def shuffle_quotes():
     # TODO write this function
     pass
 
+
 def get_offset(string):
     return (int(time.time()*SPEED) % len(string))
+
 
 def main(debug=False):
     import argparse
 
-
     parser = argparse.ArgumentParser()
     parser.add_argument('-a', '--add', type=str, help="Add a quote.")
-    parser.add_argument('-i', '--custom-id', type=str, 
-     default=False,
-     help="A custom id for the quote command. Can be used to overwrite quote.")
-    parser.add_argument('-l', '--list-quotes', action='store_true', 
-     default=False, help="List the quotes.")
-    parser.add_argument('-u', '--update_quotes', action='store_true', 
-     default=False, help="Grab quotes from twitter.")
-    parser.add_argument('-r', '--remove', type=str, 
-     help="Remove a quote by id. Prints the quote with full details if mistake. ")
+    parser.add_argument('-i', '--custom-id', type=str,
+                        default=False,
+                        help="A custom id for the quote command. Can be used "
+                        "to overwrite quote.")
+    parser.add_argument('-l', '--list-quotes', action='store_true',
+                        default=False, help="List the quotes.")
+    parser.add_argument('-u', '--update-quotes', action='store_true',
+                        default=False, help="Grab quotes from twitter.")
+    parser.add_argument('-r', '--remove', type=str,
+                        help="Remove a quote by id. Prints the quote with "
+                        "full details if mistake. ")
     args = parser.parse_args()
 
     if args.add and args.remove:
-        print("You probably shouldn't add and remove a quote at the same time.")
+        print("You probably shouldn't add and remove a quote at the same "
+              + "time.")
         print("Exiting...")
         return 0
 
@@ -128,7 +137,7 @@ def main(debug=False):
     if int(time.time()) % 14400 == 0 or args.update_quotes:
         t = auth_twitter()
         for i in TWEETERS:
-            add_quotes_from_twitter(t,i)
+            add_quotes_from_twitter(t, i)
 
     if int(time.time()) % 3600 == 0:
         shuffle_quotes()
@@ -136,21 +145,20 @@ def main(debug=False):
     preoutput = ""
     for i in quotes.keys():
         preoutput += quotes[i]['text'] + "   -   "
-        output="..."
+        output = "..."
     for i in range(len(preoutput)):
         if i < MAX_LENGTH:
-            offset=(get_offset(preoutput))
+            offset = (get_offset(preoutput))
             if debug:
                 print(i+offset-1)
                 print(len(preoutput))
                 print(preoutput)
             output += preoutput[(i+offset - 1) % (len(preoutput) - 1)]
 
-    output+='...'
-
-
+    output += '...'
 
     print(output.lower())
+
 
 if __name__ == "__main__":
     sys.exit(main())
