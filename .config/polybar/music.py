769ec0c573ecc37966504c0724f209a33186bf1d
Alvie Rahman <alvierahman90@gmail.com>
2018-07-26 15:06:07 +0100

    Clean up scripts and re add quotesbar

--- .config/polybar/music.py
+++ .config/polybar/music.py
@@ -2,37 +2,58 @@
 
 import subprocess
 
-application = 'cmus'
+APPLICATION = 'cmus'
 
 
 def player(commands):
-    return subprocess.run(['playerctl', '-p', application] + commands,
+    """
+    Returns the output of a shell command
+    Automatically adds the playerctl command and specifies music player to
+    control - cmus
+    """
+    return subprocess.run(['playerctl', '-p', APPLICATION] + commands,
                           stdout=subprocess.PIPE).stdout.decode('utf-8')
 
 
 def is_player_active():
+    """
+    Returns True when music player is open, else returns False
+    """
     return player(['status']) != 'Not available\n'
 
 
 def playing():
-    if player(['status']) == 'Playing\n':
-        return True
-    return False
+    """
+    Returns True when music is playing, else returns False
+    """
+    return player(['status']) == 'Playing\n'
 
 
 def get_artist():
+    """
+    Returns the artist of the track as string
+    """
     return player(['metadata', 'xesam:artist'])
 
 
 def get_track():
+    """
+    Returns the track name as string
+    """
     return player(['metadata', 'xesam:title'])
 
 
 def get_album():
+    """
+    Returns the album name as string
+    """
     return player(['metadata', 'xesam:album'])
 
 
 def get_album_artist():
+    """
+    Returns the artist name of an album as string
+    """
     song_artist = get_artist()
     album_artist = player(['metadata', 'xesam:albumArtist'])
     if song_artist == album_artist:
@@ -41,12 +62,18 @@ def get_album_artist():
 
 
 def status_symbol():
+    """
+    Returns a symbol based on if music is playing
+    """
     if playing():
         return '▶'
     return '⏸'
 
 
 def blinker(text):
+    """
+    Flashes text given once a second
+    """
     from time import time
 
     if int(time()) % 2 == 0:
@@ -55,6 +82,9 @@ def blinker(text):
 
 
 def main():
+    """
+    Main function when script is run
+    """
     if not is_player_active():
         print('cmus not running')
         return 0
--- .config/polybar/quotesbar.py
+++ .config/polybar/quotesbar.py
@@ -10,21 +10,29 @@ MAX_LENGTH = 100
 
 
 def auth_twitter():
+    """
+    Imports twitter api and returns authorized twitter object
+    """
     import twitter
-    with open('/home/alvie/.twitter_auth') as f:
-        creds = json.loads(f.read())
+    with open('/home/alvie/.twitter_auth') as file:
+        creds = json.loads(file.read())
 
-    t = twitter.Twitter(auth=twitter.OAuth(
+    twitter = twitter.Twitter(auth=twitter.OAuth(
         creds['token'],
         creds['token_secret'],
         creds['consumer_key'],
         creds['consumer_secret']
         ))
-    return t
+    return twitter
 
 
-def add_quotes_from_twitter(t, user):
-    tweets = t.statuses.user_timeline(screen_name=user, include_rts=False)
+def add_quotes_from_twitter():
+    """
+    Appends latest quotes from followed tweeters
+    """
+    twitter = auth_twitter()
+    for i in TWEETERS:
+        tweets = twitter.statuses.user_timeline(screen_name=i, include_rts=False)
 
     for i in tweets:
         i['text'] = i['text'].replace('\n', '    ')
@@ -32,33 +40,43 @@ def add_quotes_from_twitter(t, user):
 
 
 def get_quotes():
+    """
+    Returns all the quotes stored
+    """
     output = []
-    with open('/home/alvie/.config/polybar/quotes') as f:
-        output = json.loads(f.read())
+    with open('/home/alvie/.config/polybar/quotes') as file:
+        output = json.loads(file.read())
     return output
 
 
-# overwrite the whole thing as opposed to adding one
 def write_quotes(quotes):
-    with open('/home/alvie/.config/polybar/quotes', 'w') as f:
-        f.write(json.dumps(quotes))
+    """
+    Saves `quotes` in json format to file
+    """
+    with open('/home/alvie/.config/polybar/quotes', 'w') as file:
+        file.write(json.dumps(quotes))
 
 
-# required object format
-# "UNIQUEID" : { "text" : "TEXT" }
-
-# add a quote as opposed to overwriting the whole thing
 def write_quote(quote):
+    """
+    Adds a single quote
+    required object format
+    "UNIQUEID" : { "text" : "TEXT" }
+    """
+
     # lazy and hacky way to prevent tweets with URLs making it into my list
     if "http" in quote['text']:
-        return 1
+        return
     quotes = get_quotes()
     quotes[quote['id']] = quote
     write_quotes(quotes)
 
 
-# returns deleted message on success, return None if quote isn't found
 def remove_quote(quote_id):
+    """
+    Deletes a quote by id
+    Returns deleted message on success, return None if quote isn't found
+    """
     quotes = get_quotes()
     deleted = quotes.pop(quote_id, None)
     write_quotes(quotes)
@@ -68,15 +86,53 @@ def remove_quote(quote_id):
 # this could probably be implemented by making a list from get_quotes then
 # shuffling that in the main() function in the one place it's needed
 def shuffle_quotes():
-    # TODO write this function
+    """
+    Shuffles quotes
+    TODO write this function
+    """
     pass
 
 
 def get_offset(string):
-    return (int(time.time()*SPEED) % len(string))
+    """
+    Returns the amount the string should be offset to simulate scrolling
+    """
+    return int(time.time()*SPEED) % len(string)
+
+def add_user_quote(quotes_list, text, custom_id=False):
+    """
+    Add a custom quote, given text and optional custom id
+    """
+    quote_object = {}
+    quote_object['text'] = text
+    if custom_id:
+        quote_object['id'] = custom_id
+    else:
+        id_int = 0
+        while True:
+            quote_id = "user_" + str(id_int)
+            if quote_id not in quotes_list.keys():
+                break
+            else:
+                id_int += 1
+        quote_object['id'] = quote_id
+    write_quote(quote_object)
+    print("Written quote with ID {0}".format(quote_id))
+    return 0
+
+def quotes_as_string(quotes):
+    """
+    Returns all quotes one per line
+    """
+    string = ''
+    for i in quotes.keys():
+        string += str(quotes[i]['id']) + "\t" + quotes[i]['text'] + '\n'
 
 
 def main(debug=False):
+    """
+    Entry point when script is run
+    """
     import argparse
 
     parser = argparse.ArgumentParser()
@@ -97,32 +153,16 @@ def main(debug=False):
     if args.add and args.remove:
         print("You probably shouldn't add and remove a quote at the same "
               + "time.")
-        print("Exiting...")
         return 0
 
     quotes = get_quotes()
 
     if args.add:
-        quote_object = {}
-        quote_object['text'] = args.add
-        if args.custom_id:
-            quote_object['id'] = args.custom_id
-        else:
-            id_int = 0
-            while True:
-                quote_id = "user_" + str(id_int)
-                if quote_id not in quotes.keys():
-                    break
-                else:
-                    id_int += 1
-            quote_object['id'] = quote_id
-        write_quote(quote_object)
-        print("Written quote with ID {0}".format(quote_id))
+        add_user_quote(quotes, args.add, custom_id=args.custom_id)
         return 0
 
     if args.list_quotes:
-        for i in quotes.keys():
-            print(str(quotes[i]['id']) + "\t" + quotes[i]['text'])
+        print(quotes_as_string(quotes))
         return 0
 
     if args.remove:
@@ -135,9 +175,7 @@ def main(debug=False):
         return 0
 
     if int(time.time()) % 14400 == 0 or args.update_quotes:
-        t = auth_twitter()
-        for i in TWEETERS:
-            add_quotes_from_twitter(t, i)
+        add_quotes_from_twitter()
 
     if int(time.time()) % 3600 == 0:
         shuffle_quotes()
@@ -158,6 +196,7 @@ def main(debug=False):
     output += '...'
 
     print(output.lower())
+    return 0
 
 
 if __name__ == "__main__":
