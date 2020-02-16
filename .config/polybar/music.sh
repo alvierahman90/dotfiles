67558b36757c71e42e28e27319f2cbc310afe79f
Alvie Rahman <alvierahman90@gmail.com>
2020-02-16 19:25:03 +0000

    update polybar for mpd over cmus, replace music.py with much simpler music.sh

---
+++ .config/polybar/music.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+PLAYING_CHAR=">"
+NOT_PLAYING_CHAR="|"
+NEXT_SONG_MESSAGE=">>"
+
+NO_MUSIC_MESSAGE="No music playing"
+NO_MUSIC_EXIT_CODE=0
+
+CURRENT_SONG="$(mpc current)"
+if [ "$CURRENT_SONG" == "" ]
+then
+	echo "$NO_MUSIC_MESSAGE"
+	exit $NO_MUSIC_EXIT_CODE
+fi 
+
+PLAYING=$(mpc status | grep '\[playing\]' -c | sed -e "s/1/$PLAYING_CHAR/" | sed -e "s/0/$NOT_PLAYING_CHAR/")
+NEXT_SONG="$(mpc playlist | grep "$CURRENT_SONG" -A 1 | tail -1)"
+
+echo $PLAYING $CURRENT_SONG $NEXT_SONG_MESSAGE $NEXT_SONG
